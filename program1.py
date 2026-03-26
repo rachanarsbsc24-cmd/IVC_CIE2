c85cc33df402b971639e242216f82c219515e520
Your Name <your-email@example.com>
2026-03-26 12:15:54 +0530

    Initial Commit

---
+++ program1.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+sum_digits = 0
+
+while n > 0:
+    sum_digits += n % 10
+    n = n // 10
+
+sum_str = str(sum_digits)
+121
+
+if sum_str == sum_str[::-1]:
+    print("Sum is Palindrome")
+else:
+    print("Sum is Not Palindrome")
